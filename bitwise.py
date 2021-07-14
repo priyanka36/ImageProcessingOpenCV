7c38a4b3d16b0306249013126a2d1a3306584ecb
Priyanka Paudel <poudelnipriyanka@gmail.com>
2021-07-14 20:45:22 +0545

    More on open CV colorspaces masking

---
+++ bitwise.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2 
+rectangle =np.ones((300,300),dtype="uint8")
+cv2.rectangle(rectangle,(25,25),(275,275),255,-1)
+cv2.imshow("Rectangle",rectangle)
+
+circle = np.zeros((300,300),dtype="uint8")
+cv2.circle(circle,(150,150),150,255,-1)
+cv2.imshow("Circle",circle)
+
+
+bitwiseAnd = cv2.bitwise_and(rectangle, circle)
+cv2.imshow("AND", bitwiseAnd)
+
+
+bitwiseAndreversed = cv2.bitwise_and(circle,rectangle)
+cv2.imshow("bitwiseAndreversed",bitwiseAndreversed)
+cv2.waitKey(0)
+
+bitwiseOr =cv2.bitwise_or(rectangle,circle)
+cv2.imshow("OR",bitwiseOr)
+cv2.waitKey(0)
+
+bitwiseXor = cv2.bitwise_xor(rectangle, circle)
+cv2.imshow("XOR", bitwiseXor)
+cv2.waitKey(0)
+
+bitwiseNot = cv2.bitwise_not(circle)
+cv2.imshow("NOT", bitwiseNot)
+cv2.waitKey(0)
---
+++ test.py
@@ -0,0 +1,5 @@
+import numpy as np
+import cv2 
+rectangle =np.zeros((300,300),dtype="uint8")
+cv2.imshow("Rectangle",rectangle)
+cv2.waitKey(0)
